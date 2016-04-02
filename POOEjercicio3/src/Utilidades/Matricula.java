ea291990b389b80939137f7a8fd4a0ec3a4dc3e5
Riosfen <samo_karmo@hotmail.es>
2016-04-02 04:55:40 +0200

    Actualizando clase matricula, no terminada

--- POOEjercicio3/src/Utilidades/Matricula.java
+++ POOEjercicio3/src/Utilidades/Matricula.java
@@ -1,10 +1,53 @@
 package Utilidades;
 
+import org.monroy.clases.MatriculaException;
+
 public class Matricula {
-	
-	public Matricula (String matricula){
+
+	private String matricula;
+
+	// 4 numeros y 3 letras,
+
+	public Matricula(String matricula) throws MatriculaException {
+		matricula.trim().toUpperCase();
+		if (!comprobarCadena(matricula))
+			throw new MatriculaException("Error! matricula no válida.");
+		this.matricula = matricula;
+	}
+
+	private boolean comprobarCadena(String matricula){
+			
+		int contador = 0;
+		boolean cadenaCorrectos = true;
 		
-		// 4 numeros y 3 letras, 
+		String numeros = matricula.substring(0, 3);
+		String letras = matricula.substring(4, 6);
+		
+		while(contador < numeros.length() && cadenaCorrectos == true){
+			char c = numeros.charAt(contador);
+			if (!(Character.isDigit(c))){
+				cadenaCorrectos = false;
+			}
+			contador++;	
+		}
+		
+		contador = 0;
+		
+		while(contador < letras.length() && cadenaCorrectos == true){
+			char c = letras.charAt(contador);
+			if (Character.isDigit(c)){
+				cadenaCorrectos = false;
+			}
+			contador++;	
+		}
+		
+		return cadenaCorrectos;
+			
+	}
+	
+	public String toString(){
+		
+		return matricula;
 		
 	}
 
