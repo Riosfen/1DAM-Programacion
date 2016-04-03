a16a318f42229765f570c1102d79477039bc032c
Riosfen <samo_karmo@hotmail.es>
2016-04-03 22:17:47 +0200

    Terminado Script Matrícula.

--- POOEjercicio3/src/Utilidades/Matricula.java
+++ POOEjercicio3/src/Utilidades/Matricula.java
@@ -5,6 +5,8 @@ import org.monroy.clases.MatriculaException;
 public class Matricula {
 
 	private String matricula;
+	private static final int ABECEDARIO_INICIO = 10;
+	private static final int ABECEDARIO_FINAL = 35;
 
 	// 4 numeros y 3 letras,
 
@@ -18,30 +20,34 @@ public class Matricula {
 	private boolean comprobarCadena(String matricula){
 			
 		int contador = 0;
-		boolean cadenaCorrectos = true;
+		boolean cadenaCorrecta = true;
 		
 		String numeros = matricula.substring(0, 3);
 		String letras = matricula.substring(4, 6);
 		
-		while(contador < numeros.length() && cadenaCorrectos == true){
+		while(contador < numeros.length() && cadenaCorrecta == true){
+			
 			char c = numeros.charAt(contador);
-			if (!(Character.isDigit(c))){
-				cadenaCorrectos = false;
+			
+			if (Character.getNumericValue(c) >= ABECEDARIO_INICIO && Character.getNumericValue(c) <= ABECEDARIO_FINAL){
+				cadenaCorrecta = false;
 			}
 			contador++;	
 		}
 		
 		contador = 0;
 		
-		while(contador < letras.length() && cadenaCorrectos == true){
+		while(contador < letras.length() && cadenaCorrecta == true){
+			
 			char c = letras.charAt(contador);
+			
 			if (Character.isDigit(c)){
-				cadenaCorrectos = false;
+				cadenaCorrecta = false;
 			}
 			contador++;	
 		}
 		
-		return cadenaCorrectos;
+		return cadenaCorrecta;
 			
 	}
 	
