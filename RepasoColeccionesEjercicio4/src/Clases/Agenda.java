de21cc1f6ebe18176381e557c518c4b724dfd1a8
PC-DAM <correomentiroso@no.jaja>
2016-06-14 12:43:24 +0200

    sdfg

--- RepasoColeccionesEjercicio4/src/Clases/Agenda.java
+++ RepasoColeccionesEjercicio4/src/Clases/Agenda.java
@@ -1,5 +1,63 @@
 package Clases;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Agenda {
+
+	public LinkedList<Amigo> cAmigo;
+
+	public Agenda() {
+		cAmigo = new LinkedList<Amigo>();
+	}
+
+	public void aniadirAmigo(Amigo amigoNuevo) throws AmigoException {
+
+		int posicion = encontrarPosicion(amigoNuevo);
+
+		if (cAmigo.contains(amigoNuevo))
+			throw new AmigoException("Error, ese amigo ya existe");
+
+		cAmigo.add(posicion, amigoNuevo);
+
+	}
+
+	private int encontrarPosicion(Amigo amigoNuevo) {
+
+		boolean encontrado = false;
+		int resul = 0;
+
+		Iterator<Amigo> itr = cAmigo.iterator();
+		Amigo otro;
+
+		if (cAmigo.isEmpty()) {
+			encontrado = true;
+		} else {
+			while (itr.hasNext() && encontrado == false) {
+				otro = itr.next();
+
+				if (otro.compareTo(amigoNuevo) > 0) {
+					encontrado = true;
+				} else {
+					resul++;
+				}
+
+			}
+		}
+
+		return resul;
+	}
+
+	public void eliminarCumple(Amigo otro) {
+
+		if (cAmigo.contains(otro)) {
+			cAmigo.remove(cAmigo.indexOf(otro));
+		}
+
+	}
+
+	public void cambiarCumpleaño(String nombre) {
+
+	}
 
 }
--- RepasoColeccionesEjercicio4/src/Clases/Amigo.java
+++ RepasoColeccionesEjercicio4/src/Clases/Amigo.java
@@ -29,5 +29,50 @@ public class Amigo {
 			throw new AmigoException("Error, hora de aviso incorrecta");
 		this.horaAviso = horaAviso;
 	}
+
+	public int compareTo(Amigo otro) {
+		int resul = 0;
+	
+		resul = fecha.compareTo(otro.fecha);
+		
+		if (resul == 0){
+			if (horaAviso > otro.horaAviso){
+				resul = 1;
+			}
+			else{
+				if (otro.horaAviso > horaAviso){
+					resul = -1;
+				}
+			}
+		}
+		
+		return resul;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Amigo other = (Amigo) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
 	
 }
